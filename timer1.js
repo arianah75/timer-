078952b3de92ad230ee3c4380612efeca963ee34
Arian <arianahmadi1@gmail.com>
2023-05-10 06:51:03 -0400

    update

--- timer1.js
+++ timer1.js
@@ -1,13 +1,12 @@
 const args = process.argv.slice(2);
-//we get array of [10,3,5,15,9] these repesent seconds take for bell to go off.
-const timer = function(seconds) {
+
+const timer = (seconds) => {
   //isNaN() function takes argument and checks if it is number or not.
   if (isNaN(seconds) || seconds < 0) {
     return; // Ignore/skip negative or non-numeric input
   }
-
   setTimeout(() => {
-    process.stdout.write('\x07'); // Make a beep sound.
+    process.stdout.write("\x07"); // Make a beep sound.
   }, seconds * 1000);
 
   console.log(`Timer set for ${seconds}s`);
